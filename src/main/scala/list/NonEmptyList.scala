edfa1b0b9917e28305198b9d2320ce0a0fc69889
Tommy Sullivan <tsullivan@maprtech.com>
2017-05-01 20:02:38 -0700

    list.addToEnd

--- src/main/scala/list/NonEmptyList.scala
+++ src/main/scala/list/NonEmptyList.scala
@@ -2,7 +2,7 @@ package list
 
 case class NonEmptyList[T](firstElement:T, allOtherElements:List[T]) extends List[T] {
   override def toString = s"$firstElement,$allOtherElements"
-  override def addToEnd[S >: T](elementToAdd:S) = new NonEmptyList(firstElement, allOtherElements.addToEnd(elementToAdd))
+  override def phaddToEnd[S >: T](elementToAdd:S) = new NonEmptyList(firstElement, allOtherElements.addToEnd(elementToAdd))
   def addListToEnd[S >: T](elementsToAdd:List[S]):List[S] = {
     new NonEmptyList[S](firstElement, allOtherElements.addListToEnd(elementsToAdd))
   }
